b7b2115f8176340f68c78513cd5703a85d93b77f
Liulyatina <Roma.mihal812@gmail.com>
2024-05-25 15:02:38 +0300

    Spring boot

--- src/main/java/by/it_academy/jd2/votig/controller/http/GenreController.java
+++ src/main/java/by/it_academy/jd2/votig/controller/http/GenreController.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/genre")
 public class GenreController {
 
     private final IGenreService genreService;
